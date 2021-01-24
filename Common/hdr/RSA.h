2ff18f6a86dac2e0c1c0929113e46df8d644c02f
vililichi <55566485+vililichi@users.noreply.github.com>
2021-01-24 15:22:19 -0500

    cryptographie RSA

---
+++ Common/hdr/RSA.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "inft.h"
+
+namespace RSA
+{
+	struct cle
+	{
+		inft modulus;
+		inft exposant;
+	};
+	//génération de clés de 256 octets prive:=(d,n) public:=(e,n)
+	void generation(cle& clePrive, cle& clePublic);
+
+	//cryptage d'un paquet de 16 octets à l'aide d'une clé de 256 octets,le message et la taille de celui-ci varie
+	//prévoir un espace de de 128 octets pour le message crypté
+	void cryptage(inft& message, cle& clePublic);
+
+	//décryptage d'un paquet de 16 octets à l'aide d'une clé de 256 octets,le message et la taille de celui-ci varie
+	//message donc la taille <= 127 octets
+	void decryptage(inft& message, cle& clePrive);
+
+}
---
+++ Common/src/RSA.cpp
@@ -0,0 +1,74 @@
+#include "RSA.h"
+#include <thread>
+
+#define nbrO  128
+
+void generateRandomPrimeIn(inft* container, inft min, inft max)
+{
+    *container = randPrime(min, max);
+}
+
+void RSA::generation(cle& clePrive, cle& clePublic)
+{
+
+
+    unsigned char c[nbrO];
+    for (int i = 0; i < nbrO; i++) {
+        c[i] = 255;
+    }
+
+    inft a(c, nbrO);
+
+    inft p;
+    inft q;
+    std::thread genp(&generateRandomPrimeIn, &p, a / 2, a);
+    std::thread genq(&generateRandomPrimeIn, &q, a / 2, a);
+    genp.join();
+    genq.join();
+
+    std::cout << "p: " << p << "\n";
+    std::cout << "q: " << q << "\n";
+
+    //n et phi
+    inft n = p * q;
+    inft phi = (p - 1) * (q - 1);
+    std::cout << "phi: " << phi << "\n";
+
+    inft e;
+    inft d;
+    inft y;
+    while (true)
+    {
+        e = randinft(a / 2, a);
+        if (extendedGdc(e, phi, d, y) == 1) break;
+    }
+    if (d < 0) d = d + phi;
+
+    if (!((e * d % phi) == 1))
+    {
+        std::cout << "inversion non valide\n";
+    }
+    inft test = 10245896;
+    if (!(test.modPow(e, n).modPow(d, n) == inft(10245896)))
+    {
+        std::cout << "inversion non valide 2\n";
+    }
+
+    clePublic.modulus = n;
+    clePrive.modulus = n;
+    clePublic.exposant = e;
+    clePrive.exposant = d;
+
+}
+
+//cryptage d'un paquet de 16 octets à l'aide d'une clé de 256 octets,le message et la taille de celui-ci varie
+void RSA::cryptage(inft& message, cle& clePublic)
+{
+    message = message.modPow(clePublic.exposant, clePublic.modulus);
+}
+
+//décryptage d'un paquet de 16 octets à l'aide d'une clé de 256 octets,le message et la taille de celui-ci varie
+void RSA::decryptage(inft& message, cle& clePrive)
+{
+    message = message.modPow(clePrive.exposant, clePrive.modulus);
+}
